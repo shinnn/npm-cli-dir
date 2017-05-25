6aed984689d6b0d3672c9166208cf607e52ed514
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-05-26 01:41:52 +0900

    use resolve-from instead of pkg-dir
    to reduce bundle size

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const npmCliPath = require('npm-cli-path');
-const pkgDir = require('pkg-dir');
+const dirname = require('path').dirname;
 
-const getNpmCliDir = npmCliPath().then(pkgDir);
+const npmCliPath = require('npm-cli-path');
+const resolveFromSilent = require('resolve-from').silent;
+
+const getNpmCliDir = npmCliPath().then(result => {
+  do {
+    result = dirname(result);
+  } while (resolveFromSilent(result, './package.json') === null);
+
+  return result;
+});
 
 module.exports = function npmCliDir() {
   return getNpmCliDir;
