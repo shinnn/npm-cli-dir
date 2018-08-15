087fa1ff61056079f185442e9dafb353d41cef38
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-08-15 19:42:57 +0900

    cache the first resolution result

--- index.js
+++ index.js
@@ -3,13 +3,20 @@
 const findPkgDir = require('find-pkg-dir');
 const npmCliPath = require('npm-cli-path');
 
-let result = null;
+let path;
+const firstTry = (async () => {
+	try {
+		path = findPkgDir(await npmCliPath());
+		return true;
+	} catch (err) {
+		return false;
+	}
+})();
 
 module.exports = async function npmCliDir() {
-	if (result !== null) {
-		return result;
+	if (!path && !await firstTry) {
+		path = findPkgDir(await npmCliPath());
 	}
 
-	result = findPkgDir(await npmCliPath());
-	return result;
+	return path;
 };
